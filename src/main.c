07fe8a310ca4a4147fcb05aafb469b7edc1c2a45
damiano <damiano.lodi.93@gmail.com>
2023-01-18 23:19:51 +0100

    Configure clang-format VSCode support

--- src/main.c
+++ src/main.c
@@ -7,21 +7,23 @@
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
-void main(void) {
+int main() {
     int ret;
 
-    if (!device_is_ready(led.port)) return;
+    if (!device_is_ready(led.port))
+        return 1;
 
     ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-    if (ret < 0) {
-        return;
-    }
+    if (ret < 0)
+        return 1;
 
     while (1) {
         ret = gpio_pin_toggle_dt(&led);
-        if (ret < 0) {
-            return;
-        }
+        if (ret < 0)
+            return 1;
+
         k_msleep(SLEEP_TIME_MS);
     }
+
+    return 0;
 }
