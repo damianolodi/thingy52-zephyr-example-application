5b9cdb7bc9e20e72f2b0c9387c874924105e8529
damiano <damiano.lodi.93@gmail.com>
2023-01-29 17:08:21 +0100

    Remove overlay file

--- boards/arm/thingy52_nrf52832/board.cmake
+++ boards/arm/thingy52_nrf52832/board.cmake
@@ -4,4 +4,4 @@ if(${CMAKE_BUILD_TYPE} STREQUAL "Debug")
     list(APPEND CONF_FILE ${CMAKE_CURRENT_SOURCE_DIR}/conf/debug_rtt.conf)
 endif()
 
-list(APPEND DTC_OVERLAY_FILE ${CMAKE_CURRENT_SOURCE_DIR}/boards/arm/thingy52_nrf52832/thingy52_nrf52832.overlay)
+# list(APPEND DTC_OVERLAY_FILE ${CMAKE_CURRENT_SOURCE_DIR}/boards/arm/thingy52_nrf52832/thingy52_nrf52832.overlay)
